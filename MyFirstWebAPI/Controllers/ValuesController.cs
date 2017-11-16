f3e0b381e2210ac3bf4615cf8b30d750ecc63e12
HatimAli Johar <HatimAliJ@Cybertech.Com>
2017-11-16 16:49:11 +0530

    Add project files.

---
+++ MyFirstWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [Route("GetData")]
+    public class ValuesController : Controller
+    {
+        // Get GetData/GetHelloWorld
+        [HttpGet]
+        public string GetHelloWorld()
+        {
+            return "Hello World";
+        }
+
+        // Get GetData/1234/Provider/Query
+        [HttpGet("{key}/{SpatialOperationSource}/Query")]
+        public string GetQuery(string key, string SpatialOperationSource)
+        {
+            return "Key: " + key + " , SpatialOperationSource: " + SpatialOperationSource; 
+        }
+        
+    }
+}
